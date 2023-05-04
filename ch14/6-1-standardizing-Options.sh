41ae822252909565c45658b3891c88e3597cfd90
lacareer <chuksogu98@yahoo.com>
2023-05-04 13:02:47 -0500

    standard letter options

---
+++ ch14/6-1-standardizing-Options.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+###############################################################################
+echo 
+echo "This scripts shows various letters used as standard in command-line options"
+echo 
+######## 
+
+# -a ==> shows all object
+# -c ==> produces count
+# -d ==> specifies a directory
+# -e ==> expand an obeject
+# -f ==> specifies a file to read data f rom
+# -h ==> displays a help message for the command
+# -i ==> ignores text case
+# -n ==> uses a non-interactive (batch) mode
+# -o ==> specifies a file to which all output is redirected
+# -q ==> runs in quiet mode
+# -r ==> processes files and directories recursively
+# -s ==> runs in silent mode
+# -v ==> produces a verbose output
+# -x ==> excluddes an object
+# -y ==> answers yes to all questions
