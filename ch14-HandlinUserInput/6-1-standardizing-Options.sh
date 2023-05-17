bb4406ad94bdec8f0978fbff4fb37272fe5c10c6
lacareer <chuksogu98@yahoo.com>
2023-05-16 19:50:20 -0500

    completed ch16

--- ch14-HandlinUserInput/6-1-standardizing-Options.sh
+++ ch14-HandlinUserInput/6-1-standardizing-Options.sh
@@ -6,18 +6,19 @@ echo "This scripts shows various letters used as standard in command-line option
 echo 
 ######## 
 
-# -a ==> shows all object
-# -c ==> produces count
-# -d ==> specifies a directory
-# -e ==> expand an obeject
-# -f ==> specifies a file to read data f rom
-# -h ==> displays a help message for the command
-# -i ==> ignores text case
-# -n ==> uses a non-interactive (batch) mode
-# -o ==> specifies a file to which all output is redirected
-# -q ==> runs in quiet mode
-# -r ==> processes files and directories recursively
-# -s ==> runs in silent mode
-# -v ==> produces a verbose output
-# -x ==> excluddes an object
-# -y ==> answers yes to all questions
+echo " -a ==> shows all object"
+echo " -c ==> produces count"
+echo " -d ==> specifies a directory"
+echo " -e ==> expand an obeject"
+echo " -f ==> specifies a file to read data f rom"
+echo " -h ==> displays a help message for the command"
+echo " -i ==> ignores text case"
+echo " -n ==> uses a non-interactive (batch) mode"
+echo " -o ==> specifies a file to which all output is redirected"
+echo " -q ==> runs in quiet mode"
+echo " -r ==> processes files and directories recursively"
+echo " -s ==> runs in silent mode"
+echo " -v ==> produces a verbose output"
+echo " -x ==> excluddes an object"
+echo " -y ==> answers yes to all questions"
+echo
---
+++ ch16-scriptControl/1-linuxSignals.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+###############################################################################
+echo 
+echo "This scripts shows the various Signals in Linux"
+echo 
+######## 
+
+echo "Signal                  Value                   Description"
+echo "1                       SIGHUP                  Hang up the process"
+echo "2                       SIGINT                  Interrupts/stops the process(CTRL+C)"
+echo "3                       SIGQUIT                 Stops the process "
+echo "9                       SIGKILL                 Unconditionally terminate the process "
+echo "15                      SIGTERM                 Terminate the process if possible "
+echo "18                      SIGCONT                 Continues a stopped process"
+echo "19                      SIGSTOP                 Unconditionally stops, but dosn't terminate the process"
+echo "20                      SIGSTP                  Stops or pause the process, but doesn't terminate it(CTRL+Z)"
+echo
+
+# The Bash shell ignores signal 3 and 15 by default
+# But it does not ignore signal 1 and 2
+
+echo "      Readup ch16 of text on how to 'trap' the above signals, 
+      run a script in the abackground,
+      run a script with the 'at' command
+      and how to schedule a script using 'cron' and 'anacron"
+
+echo
